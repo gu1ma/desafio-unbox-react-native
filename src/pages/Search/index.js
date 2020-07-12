b47b6ef81bde5c7b1d9dfc0e82cf3748b840e675
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-07-12 19:37:03 -0300

    chore: inicio da pag de busca

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-import { Container } from './styles';
+import { StatusBar, FlatList, Text } from 'react-native';
+
+import api from '~/services/api';
+
+import {
+  Container,
+  Form,
+  SearchInput,
+  MovieContainer,
+  MovieImage,
+  MovieText,
+  MovieTextContainer,
+} from './styles';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Search() {
-  return <Container />;
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const searchMovies = useCallback(() => {
+    if (searchValue.lenght === 0) {
+      return;
+    }
+    const result = api.get(
+      `search/movie?api_key=dcabe3d98146057d837088eb8533a2cb&page=${page}&query=${searchValue}`,
+    );
+
+    result.then(
+      (reponse) => {
+        console.log('resp', reponse);
+        setSearchResult(reponse.data.results);
+      },
+      (err) => {
+        console.log('err', err);
+      },
+    );
+  }, [page, searchValue]);
+
+  useEffect(
+    (value) => {
+      searchMovies(value);
+      setSearchResult(value);
+      if (!value) {
+        setSearchResult([]);
+      }
+    },
+    [searchValue, searchMovies],
+  );
+
+  return (
+    <>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <Container>
+        <Form>
+          <SearchInput
+            autoCapitalize="none"
+            autoCorrect
+            placeholder="Search"
+            onChangeText={setSearchValue}
+            value={searchValue}
+          />
+        </Form>
+        <FlatList
+          data={searchResult}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => String(item.id)}
+          //onEndReached={() => loadGenres()}
+          onEndReachedThreshold={0.1}
+          //ListFooterComponent={moviesLoader && <Loader />}
+          horizontal={false}
+          renderItem={({ item }) => (
+            <MovieContainer>
+              <MovieImage
+                source={{
+                  uri: `http://image.tmdb.org/t/p/w185${item.poster_path}`,
+                }}
+              />
+              <MovieTextContainer>
+                <MovieText>{item.title}</MovieText>
+              </MovieTextContainer>
+            </MovieContainer>
+          )}
+        />
+      </Container>
+    </>
+  );
 }
 
 Search.navigationOptions = {
--- src/pages/Search/styles.js
+++ src/pages/Search/styles.js
@@ -3,4 +3,45 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   background: #111;
   flex: 1;
+  margin-top: 40px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  margin: 10px;
+`;
+
+export const SearchInput = styled.TextInput.attrs({
+  placeholderTextColor: '#ccc',
+})`
+  flex: 1;
+  padding: 5px 15px;
+  border-radius: 4px;
+  font-size: 16px;
+  color: #ccc;
+  background: #999;
+  border: #999;
+  text-align: center;
+`;
+
+export const MovieContainer = styled.View`
+  flex-direction: row;
+  margin: 10px;
+`;
+
+export const MovieImage = styled.Image`
+  width: 150px;
+  height: 80px;
+`;
+
+export const MovieTextContainer = styled.View`
+  background: #333;
+  flex: 1;
+  justify-content: center;
+  padding-left: 20px;
+`;
+
+export const MovieText = styled.Text`
+  color: white;
+  font-size: 16px;
 `;
